ab5879482d180b74f17c68cafae2359d6794ed81
Anneke Miers <annekemiers.am@gmail.com>
2020-11-06 11:06:06 -0700

    Add game constructor, game board array, wins array, and bare bones of winOrDraw function and playerTurn

---
+++ src/game.js
@@ -0,0 +1,53 @@
+class Game {
+  constructor() {
+    this.win
+  }
+}
+
+square
+var currentGame = [];
+
+var gameBoard = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8]
+];
+
+var wins = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 3, 6],
+  [2, 4, 6],
+]
+
+// Who won the game?
+function winOrDraw
+for (i = 0; i < this.wins.length; i++) {
+  if (currentGame === wins[i].length) {
+    `${player.wins}` = true;
+     `${player}score ++;
+  }
+  if (this.game !== `${wins}`) {
+  this.game.result = draw;
+  }
+  restartGame()
+}
+
+function playerTurn() {
+  if (this.game === reset) {
+    this.turn !== winner
+  } else if (this.turn === player1) {
+    this.turn = player2;
+  } else {
+    this.turn = player1;
+  }
+}
+
+restartGame(player who wins) {
+  clear gameBoard
+  
+}
