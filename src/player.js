b0ac0da706d7a1eeaf2a2edfaab433b4b88bc42e
Anneke Miers <annekemiers.am@gmail.com>
2020-11-06 11:05:09 -0700

    Add player constructor

---
+++ src/player.js
@@ -0,0 +1,10 @@
+class Player {
+  constructor(wins, icon, turn) {
+    this.wins = false;
+    this.icon = icon;
+    this.turn = false;
+  }
+}
+
+var player 1 = new Player();
+var player 2 = new Player();
